4b2ce3cdff9bd26832a654dbd0233a160b467a09
Yosuke ONOUE <onoue@likr-lab.com>
2015-05-29 17:29:19 +0900

    Add transform helper

---
+++ src/transform.js
@@ -0,0 +1,61 @@
+class Transform {
+  constructor() {
+    this.transforms = [];
+  }
+
+  translate(x, y=0) {
+    this.transforms.push({
+      type: 'translate',
+      args: [x, y]
+    });
+    return this;
+  }
+
+  scale(x, y) {
+    if (y == null) {
+      y = x;
+    }
+    this.transforms.push({
+      type: 'scale',
+      args: [x, y]
+    });
+    return this;
+  }
+
+  rotate(a, x, y) {
+    if (x != null && y != null) {
+      this.transforms.push({
+        type: 'rotate',
+        args: [a, x, y]
+      });
+    } else {
+      this.transforms.push({
+        type: 'rotate',
+        args: [a]
+      });
+    }
+    return this;
+  }
+
+  skewX(a) {
+    this.transforms.push({
+      type: 'skewX',
+      args: [a]
+    });
+    return this;
+  }
+
+  skewY(a) {
+    this.transforms.push({
+      type: 'skewY',
+      args: [a]
+    });
+    return this;
+  }
+
+  toString() {
+    return this.transforms.map(t => `${t.type}(${t.args.join(',')})`).join('');
+  }
+}
+
+export default Transform;
