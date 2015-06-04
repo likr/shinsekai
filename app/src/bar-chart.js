43d628ae3bceebbb0632cfcd9be58d463fc263d2
Yosuke ONOUE <onoue@likr-lab.com>
2015-06-04 13:16:44 +0900

    Use transform in bar-chart example

--- app/src/bar-chart.js
+++ app/src/bar-chart.js
@@ -1,6 +1,57 @@
 import angular from 'angular';
 import shinsekai from '../../src';
 
+const template = `
+<div>
+  <form>
+    <div class="form-group">
+      <label>Label</label>
+      <input class="form-control" ng-model="barChart.newLabel"/>
+    </div>
+    <div class="form-group">
+      <label>Value</label>
+      <input type="number" class="form-control" ng-model="barChart.newValue" min="0" max="100"/>
+    </div>
+  </form>
+  <div>
+    <button class="btn btn-default" ng-click="barChart.add()">Add</button>
+    <button class="btn btn-default" ng-click="barChart.clear()">Clear</button>
+  </div>
+</div>
+<div>
+  <svg ng-attr-width="{{barChart.width + 100}}" ng-attr-height="{{barChart.height + 200}}">
+    <g transform="translate(50,50)">
+      <g
+          ss-transform="barChart.transform().translate(30 * $index + 5, 0).toString()"
+          ng-repeat="datum in barChart.data">
+        <rect
+            width="20"
+            ss-y="barChart.yScale.scale(datum.value)"
+            ss-y-enter="barChart.yScale.scale(0)"
+            ss-height="barChart.yScale.scale(0) - barChart.yScale.scale(datum.value)"
+            ss-height-enter="0"
+            ss-fill="datum.color"
+            ss-dur="0.3"/>
+        <text
+            text-anchor="end"
+            ss-transform="barChart.transform().translate(20, barChart.yScale.scale(0) + 10).rotate(-60).toString()">
+          {{datum.label}}
+        </text>
+      </g>
+    </g>
+    <g transform="translate(50,50)" ss-axis="'left'" ss-ticks="10" ss-scale="barChart.yScale"/>
+    <g ss-transform="barChart.transform().translate(50, barChart.height + 50).toString()">
+      <line
+          x1="0"
+          y1="0"
+          y2="0"
+          stroke="#000"
+          ss-x2="barChart.width"/>
+    </g>
+  </svg>
+</div>
+`;
+
 const moduleName = 'shinsekai-example.bar-chart';
 
 angular.module(moduleName, [shinsekai]);
@@ -12,7 +63,7 @@ angular.module(moduleName).directive('barChart', (Transform, Scale) => {
         initialValue = 50;
   return {
     restrict: 'E',
-    templateUrl: 'components/bar-chart.html',
+    template: template,
     scope: {
     },
     controllerAs: 'barChart',
